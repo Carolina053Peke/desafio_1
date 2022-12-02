d78cff464687a7f10a02e5ae0681035ab522634d
Carolina Lemes <carolaleme21@gmail.com>
2022-12-02 01:53:11 -0300

    desafio_2

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss']
 })
-export class MainContentComponent {
+export class MainContentComponent implements OnInit {
+
+  public nombre: string;
+  public personas = ['Juan', 'Maria', 'Jose']
+
+  constructor() {}
+
+  ngOnInit(): void{
+
+  }
 
 }
